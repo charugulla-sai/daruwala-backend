5fa62eecf598d3109b8eb991bdc21542411542a6
Charugulla-Sai <satyasaicharugulla.3@gmail.com>
2024-02-28 17:51:49 +0530

    connect using atlas url

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv/config';
 
-const url = `mongodb+srv://charugullasai:${process.env.ATLAS_PASSWORD}@cluster0.pozjhoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.pozjhoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 export const connectUsingMongoose = async () => {
   try {
     await mongoose.connect(url);
-    console.log('Connect to mongodb using mongoose.');
+    console.log('Connect to mongodb using mongoose Atlas.');
   } catch (err) {
     console.log(err);
   }
